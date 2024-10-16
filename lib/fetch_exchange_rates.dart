b98feee6959ff81350c73cdb144f42763f05ce90
David Schurer <david.schurer@yahoo.com>
2024-10-15 20:52:02 -0700

    update

--- lib/fetch_exchange_rates.dart
+++ lib/fetch_exchange_rates.dart
@@ -6,48 +6,54 @@ import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 
 Future<void> main() async {
+  // Initialize Firebase
   await Firebase.initializeApp();
 
+  // Disable Firestore persistence
   FirebaseFirestore.instance.settings = Settings(persistenceEnabled: false);
 
+  // Retrieve API key from environment variable
   final String freeCurrencyAPIKey = Platform.environment['FREECURRENCYAPI_KEY'] ?? '';
 
+  // URL for fetching exchange rates
   final String url =
       'https://api.freecurrencyapi.com/v1/latest?apikey=$freeCurrencyAPIKey&currencies=USD,GBP,JPY,AUD,CAD,MXN,EUR';
-  
-    try {
-      final response = await http.get(Uri.parse(url));
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.body);
-        final rates = data['data'];
 
-        DateTime currentDate = DateTime.now();
-        String formattedDate = DateFormat('yyyy-MM-dd').format(currentDate);
+  try {
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final rates = data['data'];
 
-        for (String currency in rates.keys) {
-          double rate = rates[currency];
+      DateTime currentDate = DateTime.now();
+      String formattedDate = DateFormat('yyyy-MM-dd').format(currentDate);
 
-          final existingRate = await FirebaseFirestore.instance
-              .collection('exchange_rates')
-              .where('currency', isEqualTo: currency)
-              .where('date', isEqualTo: formattedDate)
-              .limit(1)
-              .get();
+      for (String currency in rates.keys) {
+        double rate = rates[currency];
 
-          if (existingRate.docs.isEmpty) {
-            await FirebaseFirestore.instance.collection('exchange_rates').add({
-              'currency': currency,
-              'rate': rate,
-              'date': formattedDate,
-            });
-          }
+        // Check if the exchange rate already exists in Firestore
+        final existingRate = await FirebaseFirestore.instance
+            .collection('exchange_rates')
+            .where('currency', isEqualTo: currency)
+            .where('date', isEqualTo: formattedDate)
+            .limit(1)
+            .get();
+
+        // If not, add the new rate to Firestore
+        if (existingRate.docs.isEmpty) {
+          await FirebaseFirestore.instance.collection('exchange_rates').add({
+            'currency': currency,
+            'rate': rate,
+            'date': formattedDate,
+          });
         }
-
-        print('Exchange rates fetched and stored successfully.');
-      } else {
-        print('Error fetching exchange rates: ${response.statusCode}');
       }
-    } catch (e) {
-      print('Error fetching rates: $e');
+
+      print('Exchange rates fetched and stored successfully.');
+    } else {
+      print('Error fetching exchange rates: ${response.statusCode}');
     }
+  } catch (e) {
+    print('Error fetching rates: $e');
   }
+}
